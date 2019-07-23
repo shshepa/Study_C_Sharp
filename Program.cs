efab79d55cf5c81a0296f5e888f17dadc398b8ec
shshepa <45880611+shshepa@users.noreply.github.com>
2019-07-23 20:16:50 +0300

    -m
    -m

--- Program.cs
+++ Program.cs
@@ -1,69 +1,51 @@
 ﻿using System;
-using static System.Convert;
 
 
-namespace Study_console_1
+
+namespace Second_Theme
 {
     class Program
     {
-        /*которое запрашивает у пользователя два числа в диапазоне от 0 до 255, а затем делит первое на второе. Вывод показан ниже:
-          Enter a number between 0 and 255: 100
-          Enter another number between 0 and 255: 8
-          100 divided by 8 is 12
-          Реализуйте обработчики исключений, перехватывающие любые произошедшие ошибки:
-          Enter a number between 0 and 255: apples
-          Enter another number between 0 and 255: bananas
-          FormatException: Input string was not in a correct format.
-        */
-
-        
-        static void Main(string[] args)                                                                    
+        public class FString
         {
-            bool BoolControl = false;
-            double[] NumberArray= new double[2];
-            for (int x = 0; x < 2; x++)
+            public string f, s;
+            public FString(string x, string y)
             {
-                NumberArray[x] = ConvertArea();
-                if (NumberArray[0] <= 255 && NumberArray[0] >= 0 )                                          // First value range check
-                {
-                    if(NumberArray[1] <= 255 && NumberArray[1] >= 0)                                        // Second value range check
-                    {
-                        BoolControl = true; 
-                        continue;
-                    }
-                    else
-                    {
-                        BoolControl = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    break; 
-                } 
+                f = x;
+                s = y;
             }
-            if (BoolControl == true)
-            {
-                Console.WriteLine($"{NumberArray[0] / NumberArray[1]}");
-            } 
-        }                                                                                                   
-        static double ConvertArea()                                                                         //---------------------------------                            
-        {                                                                                                   
-            double Number;                                       
-            try                                                                                             // Check format
-            {
-                string EnterValue;
-                Console.WriteLine("Enter number between 0 to 255: ");
-                EnterValue = Console.ReadLine();                                                            // Enter value from 0 to 255 and convert double
-                Number = Convert.ToDouble(EnterValue);
-                return Number;
-            }
-            catch(FormatException)
-            {
-                Console.WriteLine(" FormatException: Input string was not in a correct format.");           // If format error return invalid value
-                return 256;
-            }                                                                                               //--------
         }
+        public struct RTK
+        {
+            public int a, b, c;
+            public RTK(int q, int w, int e)
+            {
+                Console.WriteLine($"{q}----{w}----{e}");
+                while(q >= w )
+                {
+                    while (w >= e)
+                    {
+                        e++;
+                        w--;
+                    }
+                    q--;
+                    w++;
+                }
+                a = q;
+                b = w;
+                c = e;
+            }
+            
+            }
+        static void Main(string[] args)
+        {
+            FString str = new FString(Console.ReadLine(), Console.ReadLine());
+            Console.WriteLine($"{str.f} \n {str.s}");
+            RTK ineger = new RTK(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("struct");
+            Console.WriteLine($"{ineger.a}-----{ineger.b}----------{ineger.c}");
+        }
+        
     }
 }
-
+    
